d714e8c718dc2c9809714c60223460411e401375
faker007 <rlaqjadud195@naver.com>
2020-12-25 17:10:52 +0900

    [Sever/Add] Data type of Books

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNotNull,
+  GraphQLNonNull,
+} = require("graphql");
 
 const app = express();
 
@@ -62,16 +70,30 @@ const books = [
   },
 ];
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Helloworld",
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => "Hello, world!",
-      },
-    }),
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
   }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: GraphQLList(BookType),
+      description: "List of All Books",
+      resolve: () => books,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
 });
 
 app.use(
